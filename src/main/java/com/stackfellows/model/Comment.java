0c9c73ab274eb54fc7ca2ccf4b58a311b5ca6cff
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-06-13 15:25:37 -0700

    refactored Comment class per DB architecture. Added unittest.

--- src/main/java/com/stackfellows/model/Comment.java
+++ src/main/java/com/stackfellows/model/Comment.java
@@ -4,19 +4,67 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-public class Comment extends Post {
-
-    public Comment(String body) {
-        super(body);
-        // TODO: uncomment following line after merge POST class changes
-        // this.votes = 0;
-    }
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    private String body;
+    private int votes = 0;
 
     @ManyToOne
     @JoinColumn(name="post_id")
     private Post post;
 
-    @ManyToMany(cascade=CascadeType.ALL, mappedBy = "appUser")
-    private Set<AppUser> appUser;
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private AppUser appUser;
 
+    public Comment() {
+    }
+
+    public Comment(String body, Post post, AppUser appUser) {
+        this.body = body;
+        this.post = post;
+        this.appUser = appUser;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(AppUser appUser) {
+        this.appUser = appUser;
+    }
 }
--- src/test/java/com/stackfellows/StackfellowsApplicationTests.java
+++ src/test/java/com/stackfellows/StackfellowsApplicationTests.java
@@ -1,13 +1,24 @@
 package com.stackfellows;
 
+import com.stackfellows.model.Comment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class StackfellowsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testPlaceholder() {
+	}
+	@Test
+	void testCommentConstructor() {
+		String expectedResult = "Comment title.";
+		Comment sut = new Comment();
+		sut.setBody(expectedResult);
+		String actualResult = sut.getBody();
+		assertEquals(expectedResult, actualResult, "Comment should be GET-SET-able.");
 	}
 
 }
