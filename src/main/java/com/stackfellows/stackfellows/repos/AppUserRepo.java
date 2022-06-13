f86e0700ac4fea7ac9dc8fd9cf7787ddbae77faa
RogerMReyes <Rogerrmr14@gmail.com>
2022-06-13 10:26:28 -0700

    Added AppUser Model and Repo. Also added Base html files for login and signup

---
+++ src/main/java/com/stackfellows/stackfellows/repos/AppUserRepo.java
@@ -0,0 +1,8 @@
+package com.stackfellows.stackfellows.repos;
+
+import com.stackfellows.stackfellows.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepo extends JpaRepository<AppUser, Long> {
+    AppUser findByUsername(String username);
+}
