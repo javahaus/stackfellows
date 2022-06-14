4554083f488baf74aa267ebf81fb4f321cc32c76
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-06-14 13:38:28 -0700

    completed AppController display post list and renamed to HomeController

--- src/main/java/com/stackfellows/controllers/HomeController.java
+++ src/main/java/com/stackfellows/controllers/HomeController.java
@@ -7,19 +7,13 @@ import com.stackfellows.model.AppUser;
 import com.stackfellows.model.Post;
 import com.stackfellows.repos.AppUserRepo;
 import com.stackfellows.repos.PostRepo;
-import org.springframework.security.core.parameters.P;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.view.RedirectView;
+
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
-
 @Controller
-public class AppController {
+public class HomeController {
 
     @Autowired
     AppUserRepo appUserRepo;
@@ -27,17 +21,11 @@ public class AppController {
     @Autowired
     PostRepo postRepo;
 
-    @GetMapping("/blogposts")
+    @GetMapping("/index")
     public String getPosts(Principal principal, Model springModel) {
         AppUser user = appUserRepo.findByUsername(principal.getName());
         List<Post> posts = postRepo.findAll(); // TODO: consider implementing SORT in this params list
         springModel.addAttribute("posts", posts);
         return "index";
     }
-
-    @GetMapping("/index")
-    public String getHome(){
-        return "index";
-    }
-
 }
